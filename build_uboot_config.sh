be8d5cd636cc19aef7a980cd4e627d6ab3d61e3e
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 04:43:08 -0500

    Base release import: FireTV-3rd-generation-6.2.9.4-20221228

---
+++ build_uboot_config.sh
@@ -0,0 +1,22 @@
+################################################################################
+#
+#  build_uboot_config.sh
+#
+#  Copyright (c) 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+################################################################################
+
+UBOOT_SUBPATH="bootable/bootloader/uboot-amlogic/p212"
+UBOOT_DEFCONFIG_NAME="needle_defconfig"
+export TARGET_PRODUCT_NAME_NEEDLE=y
+
+# Expected image files are seperated with ":"
+UBOOT_IMAGES="fip/gxl/needle/u-boot.bin"
+
+################################################################################
+# NOTE: You must fill in the following with the path to a copy of an
+# gcc-linaro-aarch64-none-elf-4.8-2013.11_linux (aarch64-none-elf compiler) and
+# CodeSourcery g++ lite (arm-none-eabi compiler)
+################################################################################
+export PATH="$PATH:<path/to/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin>"
+export PATH="$PATH:<path/to/Sourcery_G++_Lite/bin>"
